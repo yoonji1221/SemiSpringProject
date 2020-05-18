feb0ffd9db0fcbe1da806db97d08a0a7e6c34ee4
HYOOJINN <yhjhj23@gmail.com>
2020-05-18 10:45:32 +0900

    testtest

--- WherepayProject/src/main/java/com/wherepay/wherepay/Test.java
+++ WherepayProject/src/main/java/com/wherepay/wherepay/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello wherepay");
 		System.out.println("hello");
+		System.out.println("hello2323");
+		System.out.println("hello23");
 	}
 
 }
